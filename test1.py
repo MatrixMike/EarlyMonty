3ff39272ac4696acf077deba7f7d7029ded92cbe
Mike H <matroxmike@icloud.com>
2019-10-29 15:11:22 +1100

    initial commit

---
+++ test1.py
@@ -0,0 +1,22 @@
+# mike n Pi
+
+#print("Mike H")
+
+#print ( 365 / 180)
+import turtle
+turtle.color("red")
+
+
+turtle.width(5)
+turtle.shape("turtle")
+
+def one_line():
+     turtle.forward(100)
+     turtle.right(90)
+
+
+for i in  range(1,5,1):
+     one_line()
+
+
+
